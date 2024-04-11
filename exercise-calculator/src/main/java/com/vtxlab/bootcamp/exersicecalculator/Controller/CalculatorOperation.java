913c812bf2e50490766708fdb554b9aa69c42a84
Andie.Yip <ynotrobot@gmail.com>
2024-04-11 20:36:09 +0800

    20240411 updated

--- exercise-calculator/src/main/java/com/vtxlab/bootcamp/exersicecalculator/Controller/CalculatorOperation.java
+++ exercise-calculator/src/main/java/com/vtxlab/bootcamp/exersicecalculator/Controller/CalculatorOperation.java
@@ -1,5 +1,6 @@
 package com.vtxlab.bootcamp.exersicecalculator.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +11,15 @@ import com.vtxlab.bootcamp.exersicecalculator.Model.DTO.Compute;
 
 public interface CalculatorOperation {
 
+  @CrossOrigin
   @GetMapping(value = "/calculate/{x}/{y}/{operation}")
   Compute calculate(@PathVariable String x, @PathVariable String y, @PathVariable String operation) throws Exception;
 
+  @CrossOrigin
   @PostMapping(value = "/calculate")
   Compute calculate2(@RequestBody Compute compute) throws Exception;
 
+  @CrossOrigin
   @GetMapping(value = "/calculate")
   Compute calculate3(@RequestParam(name = "x") String x, @RequestParam(name = "y") String y,
       @RequestParam(name = "operation") String operation) throws Exception;
